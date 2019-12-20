f404394d868d5abc0f7fbea84d4aa7dc1bdba09b
hamuse <hamuseco@gmail.com>
2019-12-20 13:07:24 +0900

    first commit

---
+++ src/test/java/com/hamuse/web/HamuseVue1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamuse.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HamuseVue1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
